7476ae9ea5e1c5a1a2f17ffe4f789645a1c38e48
hchau98 <chauhenry02@gmail.com>
2020-04-14 13:23:24 -0400

    First Commit

---
+++ src/CreateApp.java
@@ -0,0 +1,9 @@
+public class CreateApp {
+    public static void main(String[] args){
+        String country = "Vietnam";
+        double size = 95.54;
+        String capital = "Hanoi";
+        int convert = 21000;
+        System.out.println("Hey Mom and Dad! \n Here are some interesting details about the first country I'm visiting. \n Name: "+country+" \n Population Size: "+size+" million \n Capital: "+capital+"\n Currency: One U.S Dollar equals "+convert+" Vietnamese Dong!!");
+    }
+}
